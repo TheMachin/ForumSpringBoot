cd7d87f5857cfecc4d260583daa726f0928f5c80
David <lomre.david@gmail.com>
2017-12-20 17:36:41 +0100

    Test create find

--- src/main/java/org/miage/m2/forum/query/UtilisateurRepository.java
+++ src/main/java/org/miage/m2/forum/query/UtilisateurRepository.java
@@ -1,10 +1,11 @@
 package org.miage.m2.forum.query;
 
 import org.miage.m2.forum.modele.Utilisateur;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
-
-public interface UtilisateurRepository extends CrudRepository<Utilisateur,String> {
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
+@org.springframework.stereotype.Repository
+public interface UtilisateurRepository extends JpaRepository<Utilisateur,Long> {
+
+    public Utilisateur findByPseudo(String pseudo);
 }
--- src/test/java/org/miage/m2/forum/controller/AccountControllerTest.java
+++ src/test/java/org/miage/m2/forum/controller/AccountControllerTest.java
@@ -1,26 +1,68 @@
 package org.miage.m2.forum.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.miage.m2.forum.modele.Message;
 import org.miage.m2.forum.modele.Projet;
 import org.miage.m2.forum.modele.Topic;
+import org.miage.m2.forum.modele.Utilisateur;
+import org.miage.m2.forum.query.UtilisateurRepository;
 import org.miage.m2.forum.service.AccountService;
+import org.miage.m2.forum.service.AccountServiceImpl;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashSet;
 
 import static junit.framework.TestCase.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class) //lien entre les fonctionnalités de test Spring Boot et JUnit
 @WebMvcTest(AccountController.class)
+//@DataJpaTest // fournit des standards pour tester la persistance
 public class AccountControllerTest {
+
+    //permet de vérifier la classe Service
+    @TestConfiguration
+    static class AccountServiceImplTestContextConfiguration {
+
+        @Bean
+        public AccountService accountService() {
+            return new AccountServiceImpl();
+        }
+    }
+
+    @Autowired
+    private AccountService accountService;
+
+    // creation d'un Mock pour le UtilisateurRepository, il peut être utilisé pour ignorer son appel (bypass)
+    @MockBean
+    private UtilisateurRepository utilisateurRepository;
 
     @Test
     public final void testCreateUser(){
-        boolean test = AccountService.createUser("test@test.com", "test", "testmdp", false, new HashSet<Message>(), new HashSet<Projet>(), new HashSet<Topic>(), new HashSet<Topic>());
+        boolean test = accountService.createUser("test@test.com", "test", "testmdp", false, new HashSet<Message>(), new HashSet<Projet>(), new HashSet<Topic>(), new HashSet<Topic>());
         assertTrue(test);
+    }
+
+    @Before
+    public void setUp() {
+        Utilisateur userTest = new Utilisateur("test@test.com", "test", "testmdp", false, new HashSet<Message>(), new HashSet<Projet>(), new HashSet<Topic>(), new HashSet<Topic>());
+
+        Mockito.when(utilisateurRepository.findByPseudo(userTest.getPseudo())).thenReturn(userTest);
+    }
+
+    @Test
+    public void whenValidPseuso_thenUtilisateurFound() {
+        String pseudo = "test";
+        Utilisateur found = accountService.getUtilisateurByPseudo(pseudo);
+        assertThat(found.getPseudo()).isEqualTo(pseudo);
     }
 
 }
